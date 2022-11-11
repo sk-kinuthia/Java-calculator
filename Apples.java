63cdf3089c40a2959953eadf8ebda65ffb0a82cf
sk-kinuthia <samkariuki8841@gmail.com>
2022-11-11 12:32:26 +0300

    Java Calculator

---
+++ Apples.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+ public class Apples{
+    public static void main(String[] args){
+        Scanner bucky =new Scanner(System.in);
+        double fnum,snum,answer;
+        System.out.println("Enter first num:");
+        fnum=bucky.nextDouble();
+        System.out.println("Enter second num:");
+        snum=bucky.nextDouble();
+        answer =fnum +snum;
+        System.out.println(answer);
+    }
+ }
